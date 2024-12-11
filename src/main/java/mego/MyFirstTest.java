02b50044505f2c0805a09fc6b1eef46f33a9ba4a
shmuel mazlich <shmuelmz400@gmail.com>
2024-12-11 14:36:02 +0200

    exampleuPushToGitHab

---
+++ src/main/java/mego/MyFirstTest.java
@@ -0,0 +1,13 @@
+package mego;
+
+public class MyFirstTest {
+    public static void main(String[] args) {
+        String myFirstName = "shmouel";
+        String myLastName = "matsliach";
+        int age = 32;
+        System.out.println("hi my name is " + myFirstName + " " + myLastName);
+        System.out.println("\nmy age is " + " " + age);
+        System.out.println(60 / 3.5);
+
+    }
+}
